8abd2dd03eb4ca53092cecc814ca496c839bb78c
pupil <pupil@ivkhk.ee>
2022-09-26 08:54:09 +0300

    Task 1

---
+++ src/jktv21task1yuliiabondarenko/JKTV21Task1YuliiaBondarenko.java
@@ -0,0 +1,11 @@
+/* Task 1*/
+package jktv21task1yuliiabondarenko;
+
+/*@author pupil*/
+public class JKTV21Task1YuliiaBondarenko {
+
+    public static void main(String[] args) {
+        System.out.println("Java Hello!");
+    }
+    
+}
